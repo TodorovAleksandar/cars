c6799671bd7292e214248079b1177d378eb508c2
aleksandar <todor.petrovgrad@yahoo.com>
2017-12-21 18:59:22 +0100

    prvi commit

---
+++ resources/views/Welcome to Cars Project.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        
+        <title>Cars Project</title>
+
+    </head>
+    <body>
+       <h1> Welcome $name!</h1>
+    </body>
+</html>
---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        
+        <title>About us</title>
+
+    </head>
+    <body>
+       
+    </body>
+</html>
